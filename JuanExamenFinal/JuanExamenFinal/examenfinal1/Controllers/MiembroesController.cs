2e9d122b7aa6f3e0aa52f370033e579bd669b095
juanchord <56456671+juanchord@users.noreply.github.com>
2019-12-10 22:53:58 -0400

    ExamenFinalWeb

---
+++ JuanExamenFinal/JuanExamenFinal/examenfinal1/Controllers/MiembroesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using examenfinal1.Models;
+
+namespace examenfinal1.Controllers
+{
+    public class MiembroesController : Controller
+    {
+        private ContactosEntities1 db = new ContactosEntities1();
+
+        // GET: Miembroes
+        public ActionResult Index()
+        {
+            return View(db.Miembros.ToList());
+        }
+
+        // GET: Miembroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Miembro miembro = db.Miembros.Find(id);
+            if (miembro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(miembro);
+        }
+
+        // GET: Miembroes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Miembroes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idMiembro,m_nombre,m_apellido")] Miembro miembro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Miembros.Add(miembro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(miembro);
+        }
+
+        // GET: Miembroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Miembro miembro = db.Miembros.Find(id);
+            if (miembro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(miembro);
+        }
+
+        // POST: Miembroes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idMiembro,m_nombre,m_apellido")] Miembro miembro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(miembro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(miembro);
+        }
+
+        // GET: Miembroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Miembro miembro = db.Miembros.Find(id);
+            if (miembro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(miembro);
+        }
+
+        // POST: Miembroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Miembro miembro = db.Miembros.Find(id);
+            db.Miembros.Remove(miembro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JuanExamenFinal/JuanExamenFinal/examenfinal1/Controllers/VisitasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using examenfinal1.Models;
+
+namespace examenfinal1.Controllers
+{
+    public class VisitasController : Controller
+    {
+        private ContactosEntities1 db = new ContactosEntities1();
+
+        // GET: Visitas
+        public ActionResult Index()
+        {
+            var visitas = db.Visitas.Include(v => v.Miembro);
+            return View(visitas.ToList());
+        }
+
+        // GET: Visitas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visita visita = db.Visitas.Find(id);
+            if (visita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visita);
+        }
+
+        // GET: Visitas/Create
+        public ActionResult Create()
+        {
+            ViewBag.idMiembro = new SelectList(db.Miembros, "idMiembro", "m_nombre");
+            return View();
+        }
+
+        // POST: Visitas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idVisita,motivo_visita,nombre_completo,h_entrada,h_salida,m_recibio,idMiembro")] Visita visita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Visitas.Add(visita);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idMiembro = new SelectList(db.Miembros, "idMiembro", "m_nombre", visita.idMiembro);
+            return View(visita);
+        }
+
+        // GET: Visitas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visita visita = db.Visitas.Find(id);
+            if (visita == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idMiembro = new SelectList(db.Miembros, "idMiembro", "m_nombre", visita.idMiembro);
+            return View(visita);
+        }
+
+        // POST: Visitas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idVisita,motivo_visita,nombre_completo,h_entrada,h_salida,m_recibio,idMiembro")] Visita visita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(visita).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idMiembro = new SelectList(db.Miembros, "idMiembro", "m_nombre", visita.idMiembro);
+            return View(visita);
+        }
+
+        // GET: Visitas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visita visita = db.Visitas.Find(id);
+            if (visita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visita);
+        }
+
+        // POST: Visitas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Visita visita = db.Visitas.Find(id);
+            db.Visitas.Remove(visita);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
